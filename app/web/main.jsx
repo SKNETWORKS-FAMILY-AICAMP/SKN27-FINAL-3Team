e2fbd705439603974ad66d075b6d8ba2481a4c81
ohjuheecode <juheeoh.code@gmail.com>
2026-07-26 18:24:21 +0900

    fix: refresh frontend module cache

--- app/web/main.jsx
+++ app/web/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import FrontendAppShell from "./FrontendAppShell.jsx?mockup-v16";
-import "./styles.css?mockup-v16";
+import FrontendAppShell from "./FrontendAppShell.jsx?mockup-v17";
+import "./styles.css?mockup-v17";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
